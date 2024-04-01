d34b5d74252ab01f0cb4e8851563f2daf3f4741d
Sadaf Rao <sohailsadaf88@gmail.com>
2024-04-01 02:46:43 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,27 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+import chalk from "chalk";
+let todolist = [];
+let conditions = true;
+console.log(chalk.red.bold("\n \twell come to my todo list \n "));
+while (conditions) {
+    let addTask = await inquirer.prompt([
+        {
+            name: "task",
+            type: "input ",
+            message: chalk.green.bold("Enter your new task"),
+        }
+    ]);
+    todolist.push(addTask.task);
+    console.log(`${addTask.task} Task add in Todo_list succesfully`);
+    let addMoreTask = await inquirer.prompt([
+        {
+            name: "addmore",
+            type: "confirm",
+            message: chalk.green.bold("Do you want to add more task"),
+            default: "False",
+        }
+    ]);
+    conditions = addMoreTask.addmore;
+}
+console.log("your update Todo-list", todolist);
